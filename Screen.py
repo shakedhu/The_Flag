7641fd842b8bfca93793cb6b970d97317fbafa9f
Emily <emilymos1602@gmail.com>
2022-10-23 14:43:17 +0300

    screen

--- Screen.py
+++ Screen.py
@@ -22,8 +22,7 @@ pygame.display.flip()
 
 def green_screen():
     pygame.init()
-    imp = pygame.image.load("grass.png").convert_alpha()
-    imp.set_colorkey(white)
+    imp = pygame.image.load("grass.png").convert()
 
     imp = pygame.transform.scale(imp, (75, 50))
     for i in range(20):
@@ -53,44 +52,5 @@ def green_screen():
 green_screen()
 
 
-#random.randint(0, 9)
-
-# def draw_lose_message():
-#     draw_message(consts.LOSE_MESSAGE, consts.LOSE_FONT_SIZE,
-#                  consts.LOSE_COLOR, consts.LOSE_LOCATION)
-#
-#
-# def draw_win_message():
-#     draw_message(consts.WIN_MESSAGE, consts.WIN_FONT_SIZE,
-#                  consts.WIN_COLOR, consts.WIN_LOCATION)
-#
-#
-# def draw_message(message, font_size, color, location):
-#     font = pygame.font.SysFont(consts.FONT_NAME, font_size)
-#     text_img = font.render(message, True, color)
-#     screen.blit(text_img, location)
-
-
-# def draw_game(game_state):
-#     screen.fill(consts.BACKGROUND_COLOR)
-#     draw_arrow(game_state["rotated_arrow"])
-#
-#     if game_state["is_bubble_fired"]:
-#         draw_bubble(game_state["bullet_bubble"])
-#
-#     BubblesGrid.draw()
-#     draw_border()
-#     draw_turns(game_state["turns_left_to_add_row"])
-#     Stack.draw()
-
-    # if len(game_state["bubbles_popping"]):
-    #     BubblesGrid.animate_bubbles_pop(game_state["bubbles_popping"])
-    #     draw_bubbles_popping(game_state["bubbles_popping"])
-    #
-    # elif game_state["state"] == consts.LOSE_STATE:
-    #     draw_lose_message()
-    #
-    # elif game_state["state"] == consts.WIN_STATE:
-    #     draw_win_message()
-    #
-    # pygame.display.flip()
+def night_screen():
+    
