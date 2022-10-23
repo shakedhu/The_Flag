6ae0975afc9189580d78a07ebd3c4c06d7abf9fe
Emily <emilymos1602@gmail.com>
2022-10-23 15:22:00 +0300

    try

--- Screen.py
+++ Screen.py
@@ -1,14 +1,13 @@
 import consts
 import pygame
 import random
-import image
+import sys
 
-board_game = []
-for row in range(consts.ROWS):
-    for col in range(consts.COLS):
-        board_game.append([row, col])
-print(board_game)
-
+# board_game = []
+# for row in range(consts.ROWS):
+#     for col in range(consts.COLS):
+#         board_game.append([row, col])
+# print(board_game)
 
 #caption
 pygame.display.set_caption('flag_game')
@@ -47,10 +46,31 @@ def green_screen():
                 status = False
 
     # deactivates the pygame library
-    pygame.quit()
+    # pygame.quit()
 
-green_screen()
+# green_screen()
 
 
 def night_screen():
+    BLACK = (0, 0, 0)
+    WHITE = (200, 200, 200)
+    screen.fill(BLACK)
+
+    def drawGrid():
+        blockSize = 400  # Set the size of the grid block
+        for x in range(0, consts.WINDOW_WIDTH, blockSize):
+            for y in range(0, consts.WINDOW_HEIGHT, blockSize):
+                rect = pygame.Rect(x, y, blockSize, blockSize)
+                pygame.draw.rect(screen, WHITE, rect, 1)
+
+    while True:
+        drawGrid()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+
     
