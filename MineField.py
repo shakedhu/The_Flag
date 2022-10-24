4eb9341c9f4999bcbc10337d374177dd960b6ffd
jbt <linoyelkind@gmail.com>
2022-10-24 12:20:28 +0300

    "just beacuse"

--- MineField.py
+++ MineField.py
@@ -6,6 +6,7 @@ import sys
 start_location_solider = [[0, 0], [0, 20], [20, 0], [20, 20], [40, 0], [40, 20]] #המיקומים ההתחלתים של החיילים שלא יכולים להיות בהם מוקשים
 mine_field = []
 
+list_location_mines = []
 def night_screen():
     consts.SCREEN.fill(consts.SCREEN_MINE_COLOR)
 
@@ -18,18 +19,17 @@ def night_screen():
     def random_mines():
         pygame.init()
         imp = consts.MINE_IMG.convert()
-        list_location_mines = []
 
         imp = pygame.transform.scale(imp, (60, 20))
         for i in range(20):
             random_num_width = random.randint(0, consts.WINDOW_WIDTH - 75)
-            while random_num_width % 20 != 0:
-                # for i in start_location_solider:
-                #     if i[0] ==
-                random_num_width = random.randint(0, consts.WINDOW_WIDTH - 75)
             random_num_height = random.randint(0, consts.WINDOW_HEIGHT - 50)
-            while random_num_height % 20 != 0:
-                random_num_height = random.randint(0, consts.WINDOW_HEIGHT - 50)
+            for j in start_location_solider:
+                while random_num_width % 20 != 0 or j[0] == random_num_width:
+                        random_num_width = random.randint(0, consts.WINDOW_WIDTH - 75)
+            for f in start_location_solider:
+                while random_num_height % 20 != 0 or f[0] == random_num_height :
+                    random_num_height = random.randint(0, consts.WINDOW_HEIGHT - 50)
             list_location_mines.append([random_num_width, random_num_height])
             consts.SCREEN.blit(imp, (random_num_width, random_num_height))
 
